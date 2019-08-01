526c6c14e98c1e487abd3256a00de019b2ef13f1
Pettan93 <panhammer@seznam.cz>
2019-08-01 09:46:43 +0200

    project skeletons

---
+++ spring-backend/src/main/java/cz/kalas/samples/dogstation/DogStationApplication.java
@@ -0,0 +1,13 @@
+package cz.kalas.samples.dogstation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogStationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DogStationApplication.class, args);
+    }
+
+}
